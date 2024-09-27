4e660db2e64906486ff51df556ba2af517fa099b
whisperdancer <github.v3qz5@aliaspub.com>
2024-09-27 16:54:40 -0700

    Update pullio.sh
    Update to pullio 0.0.7

--- pullio.sh
+++ pullio.sh
@@ -5,10 +5,11 @@ DOCKER_BINARY="${DOCKER_BINARY:-$(which 'docker')}"
 CACHE_LOCATION=/tmp
 TAG=""
 DEBUG=""
-CURRENT_VERSION=0.0.6
+CURRENT_VERSION=0.0.7
 LATEST_VERSION=$(curl -fsSL "https://api.github.com/repos/hotio/pullio/releases" | jq -r .[0].tag_name)
-
+PARALLEL=1
 COMPOSE_TYPE="NONE"
+
 # Check for V1
 if [[ -n "${COMPOSE_BINARY}" ]]; then
     COMPOSE_TYPE="V1"
@@ -25,98 +26,74 @@ while [ "$1" != "" ]; do
         VALUE=$1
     fi
     case $PARAM in
-        --tag)
-            [[ -n $VALUE ]] && [[ $VALUE != "--"* ]] && TAG=".$VALUE"
-            ;;
-        --debug)
-            [[ $VALUE != "--"* ]] && DEBUG="${VALUE:-debug}"
-            ;;
+    --tag)
+        [[ -n $VALUE ]] && [[ $VALUE != "--"* ]] && TAG=".$VALUE"
+        ;;
+    --debug)
+        [[ $VALUE != "--"* ]] && DEBUG="${VALUE:-debug}"
+        ;;
+    --parallel)
+        [[ $VALUE =~ ^[0-9]+$ ]] && PARALLEL=$VALUE
+        ;;
     esac
     shift
 done
 
-echo "Running with \"DEBUG=$DEBUG\" and \"TAG=$TAG\"."
+echo "Running with \"DEBUG=$DEBUG\", \"TAG=$TAG\", and \"PARALLEL=$PARALLEL\"."
 echo "Current version: ${CURRENT_VERSION}"
 echo "Latest version: ${LATEST_VERSION}"
+
+# Setups the environment variables
+setup_environment() {
+    # Always export these variables
+    export COMPOSE_BINARY DOCKER_BINARY CACHE_LOCATION TAG DEBUG COMPOSE_TYPE
+
+    # Export functions and additional variables only for parallel execution
+    if [ "$PARALLEL" -gt 1 ]; then
+        export -f process_container compose_pull_wrapper compose_up_wrapper
+        export -f send_discord_notification send_gotify_notification send_generic_webhook export_env_vars
+        export sum
+    fi
+}
 
 compose_pull_wrapper() {
     cd "$1" || exit 1
     case $COMPOSE_TYPE in
-        "V1")
-            "${COMPOSE_BINARY}" pull "$2"
-            ;;
-        "V2")
-            "${DOCKER_BINARY}" compose pull "$2"
-            ;;
-        "NONE")
-            if [[ -n "${DOCKER_BINARY}" ]]; then
-                "${DOCKER_BINARY}" run --rm -v /var/run/docker.sock:/var/run/docker.sock -v "$1:$1" -w="$1" linuxserver/docker-compose pull "$2"
-            else
-                echo "Error: Neither Docker Compose nor Docker binary is available. Cannot pull." >&2
-                return 1
-            fi
-            ;;
+    "V1")
+        "${COMPOSE_BINARY}" pull "$2"
+        ;;
+    "V2")
+        "${DOCKER_BINARY}" compose pull "$2"
+        ;;
+    "NONE")
+        if [[ -n "${DOCKER_BINARY}" ]]; then
+            "${DOCKER_BINARY}" run --rm -v /var/run/docker.sock:/var/run/docker.sock -v "$1:$1" -w="$1" linuxserver/docker-compose pull "$2"
+        else
+            echo "Error: Neither Docker Compose nor Docker binary is available. Cannot pull." >&2
+            return 1
+        fi
+        ;;
     esac
 }
 
 compose_up_wrapper() {
     cd "$1" || exit 1
     case $COMPOSE_TYPE in
-        "V1")
-            "${COMPOSE_BINARY}" up -d --always-recreate-deps "$2"
-            ;;
-        "V2")
-            "${DOCKER_BINARY}" compose up -d --always-recreate-deps "$2"
-            ;;
-        "NONE")
-            if [[ -n "${DOCKER_BINARY}" ]]; then
-                "${DOCKER_BINARY}" run --rm -v /var/run/docker.sock:/var/run/docker.sock -v "$1:$1" -w="$1" linuxserver/docker-compose up -d --always-recreate-deps "$2"
-            else
-                echo "Error: Neither Docker Compose nor Docker binary is available. Cannot bring up services." >&2
-                return 1
-            fi
-            ;;
+    "V1")
+        "${COMPOSE_BINARY}" up -d --always-recreate-deps "$2"
+        ;;
+    "V2")
+        "${DOCKER_BINARY}" compose up -d --always-recreate-deps "$2"
+        ;;
+    "NONE")
+        if [[ -n "${DOCKER_BINARY}" ]]; then
+            "${DOCKER_BINARY}" run --rm -v /var/run/docker.sock:/var/run/docker.sock -v "$1:$1" -w="$1" linuxserver/docker-compose up -d --always-recreate-deps "$2"
+        else
+            echo "Error: Neither Docker Compose nor Docker binary is available. Cannot bring up services." >&2
+            return 1
+        fi
+        ;;
     esac
-}
-
-send_gotify_notification() {
-    if [[ "${LATEST_VERSION}" != "${CURRENT_VERSION}" ]]; then
-        footer_text="Powered by Pullio (update available)"
-    else
-        footer_text="Powered by Pullio"
-    fi
-    extra=""
-    if [[ -n $3 ]] && [[ -n $4 ]] && [[ -n $7 ]] && [[ -n $8 ]]; then
-        v_ind=">" && [[ ${3} == "${4}" ]] && v_ind="="
-        r_ind=">" && [[ ${7} == "${8}" ]] && r_ind="="
-        extra=',
-            {
-            "name": "Version",
-            "value": "```\n'${3}'\n ='$v_ind' '${4}'```"
-            },
-            {
-            "name": "Revision (Git SHA)",
-            "value": "```\n'${7:0:6}'\n ='$r_ind' '${8:0:6}'```"
-            }'
-    fi
-    d_ind=">" && [[ ${9} == "${10}" ]] && d_ind="="
-    author_url="${12}" && [[ -z ${12} ]] && author_url="https://github.com/hotio/pullio/raw/master/pullio.png"
-
-    PRIORITY=5
-    name="${2}"
-    description="**${1}**"
-    image="${5}"
-    image_id="\n${9:0:11}\n =$d_ind ${10:0:11}"
-    version="\n${3}\n =$v_ind ${4}"
-    revision="\n${7:0:6}\n =$r_ind ${8:0:6}"
-    NOW=$(date +"%m-%d-%Y %r")
-      
-    printf -v m "%b\n\n**Image:**\n%b\n\n**Image Id**%b\n\n**Version**:%b\n\n**Revision (Git SHA):**%b\n\n%b\n**%s**\n" \
-     "${description}" "${image}" "${image_id}" "${version}" "${revision}" "${footer_text}" "${NOW}"
-
-    jsonMessage=$(jq -n --arg title "$name" --arg 'message' "$m" --arg priority "$PRIORITY" '{message: $message, priority: $priority | tonumber, title: $title, "extras": {"client::display": {"contentType": "text/markdown"}}}')
-
-    curl -s -S --json "$jsonMessage" -H 'Content-Type: application/json' "${6}" > /dev/null 2>&1
 }
 
 send_discord_notification() {
@@ -173,6 +150,48 @@ send_discord_notification() {
     curl -fsSL -H "User-Agent: Pullio" -H "Content-Type: application/json" -d "${json}" "${6}"
 }
 
+# add gotify notication function
+send_gotify_notification() {
+    if [[ "${LATEST_VERSION}" != "${CURRENT_VERSION}" ]]; then
+        footer_text="Powered by Pullio (update available)"
+    else
+        footer_text="Powered by Pullio"
+    fi
+    extra=""
+    if [[ -n $3 ]] && [[ -n $4 ]] && [[ -n $7 ]] && [[ -n $8 ]]; then
+        v_ind=">" && [[ ${3} == "${4}" ]] && v_ind="="
+        r_ind=">" && [[ ${7} == "${8}" ]] && r_ind="="
+        extra=',
+            {
+            "name": "Version",
+            "value": "```\n'${3}'\n ='$v_ind' '${4}'```"
+            },
+            {
+            "name": "Revision (Git SHA)",
+            "value": "```\n'${7:0:6}'\n ='$r_ind' '${8:0:6}'```"
+            }'
+    fi
+    d_ind=">" && [[ ${9} == "${10}" ]] && d_ind="="
+    author_url="${12}" && [[ -z ${12} ]] && author_url="https://github.com/hotio/pullio/raw/master/pullio.png"
+
+    PRIORITY=5
+    name="${2}"
+    description="**${1}**"
+    image="${5}"
+    image_id="\n${9:0:11}\n =$d_ind ${10:0:11}"
+    version="\n${3}\n =$v_ind ${4}"
+    revision="\n${7:0:6}\n =$r_ind ${8:0:6}"
+    NOW=$(date +"%m-%d-%Y %r")
+      
+    printf -v m "%b\n\n**Image:**\n%b\n\n**Image Id**%b\n\n**Version**:%b\n\n**Revision (Git SHA):**%b\n\n%b\n**%s**\n" \
+     "${description}" "${image}" "${image_id}" "${version}" "${revision}" "${footer_text}" "${NOW}"
+
+    jsonMessage=$(jq -n --arg title "$name" --arg 'message' "$m" --arg priority "$PRIORITY" '{message: $message, priority: $priority | tonumber, title: $title, "extras": {"client::display": {"contentType": "text/markdown"}}}')
+
+    curl -s -S --json "$jsonMessage" -H 'Content-Type: application/json' "${6}" > /dev/null 2>&1
+
+}
+
 send_generic_webhook() {
     json='{
     "container": "'${2}'",
@@ -206,12 +225,8 @@ export_env_vars() {
     export PULLIO_AUTHOR_URL=${13}
 }
 
-sum="$(sha1sum "$0" | awk '{print $1}')"
-
-mapfile -t containers < <("${DOCKER_BINARY}" ps --format '{{.Names}}' | sort -k1 | awk '{ print $1 }')
-
-for i in "${!containers[@]}"; do
-    IFS=" " read -r container_name <<< "${containers[i]}"
+process_container() {
+    local container_name="$1"
     echo "$container_name: Checking..."
 
     image_name=$("${DOCKER_BINARY}" inspect --format='{{.Config.Image}}' "$container_name")
@@ -227,6 +242,7 @@ for i in "${!containers[@]}"; do
     pullio_update=$("${DOCKER_BINARY}" inspect --format='{{ index .Config.Labels "org.hotio.pullio'"${TAG}"'.update" }}' "$container_name")
     pullio_notify=$("${DOCKER_BINARY}" inspect --format='{{ index .Config.Labels "org.hotio.pullio'"${TAG}"'.notify" }}' "$container_name")
     pullio_discord_webhook=$("${DOCKER_BINARY}" inspect --format='{{ index .Config.Labels "org.hotio.pullio'"${TAG}"'.discord.webhook" }}' "$container_name")
+    # add gotify webhook
     pullio_gotify_webhook=$("${DOCKER_BINARY}" inspect --format='{{ index .Config.Labels "org.hotio.pullio'"${TAG}"'.gotify.webhook" }}' "$container_name")
     pullio_generic_webhook=$("${DOCKER_BINARY}" inspect --format='{{ index .Config.Labels "org.hotio.pullio'"${TAG}"'.generic.webhook" }}' "$container_name")
     pullio_script_update=($("${DOCKER_BINARY}" inspect --format='{{ index .Config.Labels "org.hotio.pullio'"${TAG}"'.script.update" }}' "$container_name"))
@@ -235,10 +251,10 @@ for i in "${!containers[@]}"; do
     pullio_author_avatar=$("${DOCKER_BINARY}" inspect --format='{{ index .Config.Labels "org.hotio.pullio'"${TAG}"'.author.avatar" }}' "$container_name")
     pullio_author_url=$("${DOCKER_BINARY}" inspect --format='{{ index .Config.Labels "org.hotio.pullio'"${TAG}"'.author.url" }}' "$container_name")
 
-    if [[ ( -n $docker_compose_version ) && ( $pullio_update == true || $pullio_notify == true ) ]]; then
+    if [[ (-n $docker_compose_version) && ($pullio_update == true || $pullio_notify == true) ]]; then
         if [[ -f $pullio_registry_authfile ]]; then
             echo "$container_name: Registry login..."
-            jq -r .password < "$pullio_registry_authfile" | "${DOCKER_BINARY}" login --username "$(jq -r .username < "$pullio_registry_authfile")" --password-stdin "$(jq -r .registry < "$pullio_registry_authfile")"
+            jq -r .password <"$pullio_registry_authfile" | "${DOCKER_BINARY}" login --username "$(jq -r .username <"$pullio_registry_authfile")" --password-stdin "$(jq -r .registry <"$pullio_registry_authfile")"
         fi
 
         echo "$container_name: Pulling image..."
@@ -267,17 +283,17 @@ for i in "${!containers[@]}"; do
                 status_generic="update_success"
                 color=3066993
             else
-                echo "$container_name: Updating container failed!"
+                echo "${container_name}: Updating container failed!"
                 status="I tried to update myself.\nIt didn't work out, I might need some help."
                 status_generic="update_failure"
                 color=15158332
             fi
-            rm -f "$CACHE_LOCATION/$sum-$container_name.notified"
+            rm -f "${CACHE_LOCATION}/${sum}-${container_name}.notified"
         fi
 
         if [[ "${image_digest}" != "$container_image_digest" ]] && [[ $pullio_notify == true ]]; then
-            touch "$CACHE_LOCATION/$sum-$container_name.notified"
-            notified_digest=$(cat "$CACHE_LOCATION/$sum-$container_name.notified")
+            touch "${CACHE_LOCATION}/${sum}-${container_name}.notified"
+            notified_digest=$(cat "${CACHE_LOCATION}/${sum}-${container_name}.notified")
             if [[ $notified_digest != "$image_digest" ]]; then
                 if [[ -n "${pullio_script_notify[*]}" ]]; then
                     echo "$container_name: Executing notify script..."
@@ -292,15 +308,35 @@ for i in "${!containers[@]}"; do
                     echo "$container_name: Sending generic webhook..."
                     send_generic_webhook "$status_generic" "$container_name" "$old_opencontainers_image_version" "$new_opencontainers_image_version" "$image_name" "$pullio_generic_webhook" "$old_opencontainers_image_revision" "$new_opencontainers_image_revision" "${container_image_digest/sha256:/}" "${image_digest/sha256:/}" "$pullio_author_avatar" "$pullio_author_url"
                 fi
+                # add gotity processing
                 if [[ -n "$pullio_gotify_webhook" ]]; then
                     echo "$container_name: Sending gotify notification..."
                     send_gotify_notification "$status" "$container_name" "$old_opencontainers_image_version" "$new_opencontainers_image_version" "$image_name" "$pullio_gotify_webhook" "$old_opencontainers_image_revision" "$new_opencontainers_image_revision" "${container_image_digest/sha256:/}" "${image_digest/sha256:/}" "$color" "$pullio_author_avatar" "$pullio_author_url"
                 fi
-                echo "$image_digest" > "$CACHE_LOCATION/$sum-$container_name.notified"
+                echo "$image_digest" >"${CACHE_LOCATION}/${sum}-${container_name}.notified"
             fi
         fi
     fi
-done
+}
+
+# Respect ctrl+c
+trap 'exit 130' INT
+
+# Setup the environment
+sum="$(sha1sum "$0" | awk '{print $1}')"
+declare -a containers
+readarray -t containers < <("${DOCKER_BINARY}" ps --format '{{.Names}}' | sort -k1)
+setup_environment
+
+echo "Processing ${#containers[@]} containers with parallelism of $PARALLEL"
+
+if [ "$PARALLEL" -gt 1 ]; then
+    printf '%s\n' "${containers[@]}" | xargs -P "$PARALLEL" -I {} bash -c 'process_container "$@"' _ {}
+else
+    for container_name in "${containers[@]}"; do
+        process_container "$container_name"
+    done
+fi
 
 echo "Pruning docker images..."
 "${DOCKER_BINARY}" image prune --force
